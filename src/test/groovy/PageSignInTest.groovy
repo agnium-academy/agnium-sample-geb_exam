f20c120bf0a3acdec6fed0e200b72499b5d8e2e6
c07hn00jdjd0 <rapiudin.rapii@photoninfotech.net>
2015-10-21 17:18:01 +0700

    PageSignInTest.groovy has been edited.

--- src/test/groovy/PageSignInTest.groovy
+++ src/test/groovy/PageSignInTest.groovy
@@ -24,7 +24,7 @@ class PageSignInTest extends GebReportingTest {
     void testSignInWith(String username, String password, String indicator) {
         psi = to(PageSignIn)
         psi.moduleSignIn.signIn(username, password)
-        assert psi.moduleSignIn.flashMessage == indicator
+        assert psi.moduleSignIn.flashMessage(indicator)
     }
 
     /**
@@ -35,7 +35,7 @@ class PageSignInTest extends GebReportingTest {
      */
     @Test
     void testSignInInvalidCredentials() {
-        testSignInWith('username', 'password', 'Incorrect username or password!!!')
+        testSignInWith('agnium', 'invalidpassword', 'Incorrect username or password!!!')
     }
 
 }
