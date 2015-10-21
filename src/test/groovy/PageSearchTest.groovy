8a72c06ab0d7c11e584c03b8e5a5b0d62b3fdd83
c07hn00jdjd0 <rapiudin.rapii@photoninfotech.net>
2015-10-21 17:17:46 +0700

    PageSearchTest.groovy has been edited.

--- src/test/groovy/PageSearchTest.groovy
+++ src/test/groovy/PageSearchTest.groovy
@@ -23,7 +23,7 @@ class PageSearchTest extends GebReportingTest {
     void testSearchFor(String keyword, String repoResult) {
         ps = to(PageSearch)
         ps.moduleSearch.searchWithKeyword(keyword)
-        assert ps.moduleSearch.firstSearchResultText == repoResult
+        assert ps.moduleSearch.firstSearchResultText(repoResult)
         ps.moduleSearch.firstSearchResult.click()
     }
 
@@ -33,7 +33,7 @@ class PageSearchTest extends GebReportingTest {
      */
     @Test
     void testSearchForGit() {
-        testSearchFor('git', 'git')
+        testSearchFor('git', 'git/git')
     }
 
     /**
